069afb2296e47d10555bf6b522a55cdfa312bb66
suzu92 <92sustho@gafe.molndal.se>
2021-12-03 16:06:58 +0100

    bytte namn

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+let bildIndex = 1;
+showImg(bildIndex);
 function plusSlides(n) {
-    showSlides(slideIndex += n);
+    showImg(bildIndex += n);
 }
 function currentSlide(n) {
-    showSlides(slideIndex = n);
+    showImg(bildIndex = n);
 }
-function showSlides(n) {
+function showImg(n) {
     let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    let imgs = document.getElementsByClassName("minaBilder");
+    let punkter = document.getElementsByClassName("punkt");
+    if (n > imgs.length) {bildIndex = 1}
+    if (n < 1) {bildIndex = imgs.length}
+    for (i = 0; i < imgs.length; i++) {
+        imgs[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    for (i = 0; i < punkter.length; i++) {
+        punkter[i].className = punkter[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    imgs[bildIndex-1].style.display = "block";
+    punkter[bildIndex-1].className += " active";
 }
